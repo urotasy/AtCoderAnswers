9cbd45fb1ce35967745a711427060667c6e893af
urotasy <yonezawa.satoru.6615@gmail.com>
2020-09-20 20:07:17 +0900

    Add ABC016A.cpp, ABC016B.cpp

---
+++ ABC016/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (void) {
+    int M, D;
+    cin >> M >> D;
+
+    if ( M % D == 0 )   cout << "YES" << endl;
+    else                cout << "NO" << endl;
+    return 0;
+}
---
+++ ABC016/B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (void) {
+    int A, B, C;
+    cin >> A >> B >> C;
+
+    if ( A + B == C && A - B == C ) cout << "?" << endl;
+    else if ( A + B == C )          cout << "+" << endl;
+    else if ( A - B == C )          cout << "-" << endl;
+    else                            cout << "!" << endl;
+    return 0;
+}
