5d0b5b551fa74df30914b8908e04dd0df59dc04c
urotasy <yonezawa.satoru.6615@gmail.com>
2020-11-15 20:18:26 +0900

    Add ABC178A.cpp, ABC178B.cpp

---
+++ ABC178/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main (void) {
+    int x;
+    cin >> x;
+
+    if ( x == 0 ) cout << 1 << endl;
+    else if ( x == 1 ) cout << 0 << endl;
+    return 0;
+}
